c25131ab46c139877297041f236904712c55c119
Kittanokas <kitai1012@gmail.com>
2021-10-23 15:51:01 +0900

    first commit

---
+++ app/helpers/protorypes_helper.rb
@@ -0,0 +1,2 @@
+module ProtorypesHelper
+end
---
+++ test/controllers/protorypes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProtorypesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
